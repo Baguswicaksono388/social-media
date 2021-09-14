4fed6d70b3e210bb31d07d933f5ee5025b666fe5
Bagus <sgiapi1@gmail.com>
2021-09-14 09:37:31 +0700

    API Unfollow

--- server/routes/users.js
+++ server/routes/users.js
@@ -69,7 +69,7 @@ router.put('/:id/follow', async (req, res) => {
                 await currentUser.updateOne({ $push: { followings: req.params.id } });
                 res.status(200).json({ message: "user has been followed" });
             } else {
-                res.status(403).json({ message:"you already follow this user" });
+                res.status(403).json({ message: "you already follow this user" });
             }
         } catch (error) {
             console.log(error);
@@ -77,6 +77,27 @@ router.put('/:id/follow', async (req, res) => {
         }
     } else {
         res.status(403).json({ message: "You can follow yourself" });
+    }
+});
+
+router.put('/:id/unfollow', async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { followings: req.params.id } });
+                res.status(200).json({ message: "user has been unfollowed" });
+            } else {
+                res.status(403).json({ message:"you dont follow this user" });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Error" });
+        }
+    } else {
+        res.status(403).json({ message: "You can unfollow yourself" });
     }
 })
 
