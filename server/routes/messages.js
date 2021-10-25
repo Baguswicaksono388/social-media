06f64893470a119e8c44a54d8e48dab88aa9d979
Bagus <sgiapi1@gmail.com>
2021-10-25 17:36:49 +0700

    API Get Message By ConversationId

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -11,7 +11,18 @@ router.post("/", async (req, res) => {
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
+
 // get
+router.get("/:conversationId", async (req, res) => {
+    try {
+        const messages = await Message.find({
+            conversationId: req.params.conversationId
+        });
+        res.status(200).json(messages);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 module.exports = router;
